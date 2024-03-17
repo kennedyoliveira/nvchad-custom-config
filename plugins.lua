4d5aeb879141a0ccfb3e0229dad7f9ba4b17fda8
Kennedy Oliveira <kennedyroliveira@gmail.com>
2024-03-17 00:07:23 -0300

    Add neogit

--- plugins.lua
+++ plugins.lua
@@ -130,6 +130,23 @@ local plugins = {
     },
   },
 
+  -- Neogit https://github.com/NeogitOrg/neogit?tab=readme-ov-file
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",  -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      "nvim-telescope/telescope.nvim", -- optional
+      -- "ibhagwan/fzf-lua",              -- optional
+    },
+    config = true,
+    cmd = {
+      "Neogit",
+    }
+  },
+
   -- end
 }
 
